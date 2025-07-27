1bd49c0f9373f0d514bbdf0cea6dced62e80d498
Jonatas <jonatas.jorge@live.com>
2025-07-27 16:52:09 +0000

    Feito e corrigido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,21 +5,23 @@ int main() {
     char Estado[20];
     char codigo [20];
     char cidade [20];
-    int População, Turisticos;
-    float Area, Pib;
+    int População;
+    int Turisticos;
+    float Area;
+    float Pib;
 
     printf("Carta 1 \n");
     printf("Estado: \n");
-    scanf("%s %s", &Estado);
+    scanf(" %s", Estado);
 
     printf("Código da Carta: \n");
-    scanf("%s %s", &codigo);
+    scanf(" %s", codigo);
 
     printf("Nome da cidade:\n");
-    scanf("%s %s", &cidade);
+    scanf(" %s", cidade);
 
      printf("População: \n");
-     scanf("%f", &População);
+     scanf("%d", &População);
 
      printf("Área em KM²: \n");
      scanf("%f", &Area);
@@ -30,19 +32,18 @@ int main() {
      printf("Número de pontos turisticos: \n");
      scanf("%d", &Turisticos);
 
-
     printf("Carta 2 \n");
     printf("Estado: \n");
-    scanf("%s %s", &Estado);
+    scanf(" %s", Estado);
 
     printf("Código da Carta: \n");
-    scanf("%s %s", &codigo);
+    scanf(" %s", codigo);
 
     printf("Nome da cidade:\n");
-    scanf("%s %s", &cidade);
+    scanf(" %s", cidade);
 
      printf("População: \n");
-     scanf("%f", &População);
+     scanf(" %d", &População);
 
      printf("Área em KM²: \n");
      scanf("%f", &Area);
